7b1214ad484bdc26ce5a9e70e4a19b4b2e3e89d5
delPabloShostak <shostakevich.ak16@gmail.com>
2021-03-12 22:10:18 +0200

    3 hundred bugs

---
+++ Assets/Script/FPSInput.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float gravity = -9.8f;
+    private int count;
+    private int countCoub;
+    private CharacterController _charController;
+    public Text countText, leftText, winText;
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+        count = 0;
+        winText.text = "";
+        countCoub = GameObject.FindGameObjectsWithTag("gun").Length;
+        setCount();
+    }
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        float deltaZ = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "gun")
+        {
+            Destroy(other.gameObject);
+            count++;
+            setCount();
+        }
+    }
+    private void setCount()
+    {
+        countText.text = " Кількість :" + count.ToString();
+        leftText.text = "Залишилось :" + (countCoub - count).ToString();
+        if (count >= countCoub)
+            winText.text = "Ліс зачищено!";
+    }
+}
---
+++ Assets/Script/Rotate.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Rotate : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 35, 0) * Time.deltaTime);
+    }
+}
